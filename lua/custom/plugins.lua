6aa8861c746dff56194b8873745e07c572369e08
TheGreenPlanet <48806927+TheGreenPlanet@users.noreply.github.com>
2024-01-16 15:03:41 +0100

    Add harpoon

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -28,12 +28,50 @@ local plugins = {
   },
   {
     "ThePrimeagen/harpoon",
-    lazy = false,
+    lazy = true,
     branch = "harpoon2",
     dependencies = { "nvim-lua/plenary.nvim" },
-    opts = function ()
-      return require "custom.configs.harpoon"
+    config = function()
+      require("harpoon"):setup()
     end,
+    keys = {
+      { "<leader>a",
+      function ()
+        require("harpoon"):list():append()
+      end,
+      desc = "Harpoon Add",
+      },
+      { "<leader>m",
+        function ()
+          require("harpoon").ui:toggle_quick_menu(require("harpoon"):list())
+        end,
+        desc = "Harpoon Menu",
+      },
+      { "äj",
+        function ()
+          require("harpoon"):list():select(1)
+        end,
+        desc = "Harpoon Select 1"
+      },
+        { "äk",
+        function ()
+          require("harpoon"):list():select(2)
+        end,
+        desc = "Harpoon Select 2"
+      },
+      {"äl",
+        function ()
+          require("harpoon"):list():select(3)
+        end,
+        desc = "Harpoon Select 3"
+      },
+        { "äö",
+        function ()
+          require("harpoon"):list():select(4)
+        end,
+        desc = "Harpoon Select 4"
+      },
+    },
   },
   {
     "neovim/nvim-lspconfig",
